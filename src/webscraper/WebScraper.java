b8847cf3bd4a6de982baa83e42c2d60d4a1e1a16
Peter <p-meisel@hotmail.com>
2020-03-19 13:13:27 +0100

    Opens and gets info, can open second windows

---
+++ src/webscraper/WebScraper.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webscraper;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import static org.openqa.grid.common.SeleniumProtocol.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import webscraper.navigator.Navigator;
+
+/**
+ *
+ * @author Peter
+ */
+public class WebScraper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static  void main(String[] args) throws IOException, InterruptedException {
+        
+        Navigator navigator = new Navigator();
+        navigator.run();
+        // TODO code application logic here
+
+//
+//WebDriver driver = null;
+//
+    /*    WebDriver driver = null;
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Peter\\Documents\\NetBeansProjects\\WebScraper\\Driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        String siteToVisit = "https://www.avanza.se/fonder/lista.html?fundType=Aktiefond&sortField=developmentOneYear&sortDirection=DESCENDING&selectedTab=overview";
+      
+     //   driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); //affects how long it stays on the site
+        
+        String title = driver.getTitle();
+        System.out.println(title);
+        WebElement element = driver.findElement(By.cssSelector("body > aza-app > div > main > div > aza-fund-list > aza-subpage > div > div > div"));
+        String tagName = element.getText();
+        System.out.println(tagName);
+        System.out.println("as");
+
+
+      
+
+   
+    }
+
+  
+    
+    public void connectToThisSiteWithThisWebdriver(String site, WebDriver thisDriver){
+        thisDriver.navigate().to(site);*/
+        
+    }
+    
+    
+
+}
