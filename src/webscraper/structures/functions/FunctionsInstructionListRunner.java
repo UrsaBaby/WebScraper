79b032f96117e512b651d9cefabd74207c3a5690
Peter <p-meisel@hotmail.com>
2020-08-31 11:37:28 +0200

    Refactoring FunctionsInstructionListRunner.

--- src/webscraper/structures/functions/FunctionsInstructionListRunner.java
+++ src/webscraper/structures/functions/FunctionsInstructionListRunner.java
@@ -20,49 +20,64 @@ public class FunctionsInstructionListRunner implements Runnable {
     ListOfFunctionInstructions listToRun;
     LogicInterface logicInterface;
     Functions functions;
+    WebFunction webFunctions;
 
     public FunctionsInstructionListRunner(ListOfFunctionInstructions listToRun) throws AWTException {
         this.listToRun = listToRun;
         logicInterface = new LogicInterface();
-        functions = new Functions("FunctionsInstructionListRunner");
     }
 
     @Override
     public void run() {
         try {
-            for (FunctionsInstruction checker : listToRun.getListOfFunctionInstruction()) {
-                switch (checker.getCommand()) {
-                    case CLICKWEBELEMENT:
-                        functions.clickElementAtThisCss(checker.getStringValueWithIndex(0));
-                        break;
-                    case CONNECTTOTHISITE:
-                        functions.connectWebGetterToThisSite(checker.getStringValueWithIndex(0));
-                        break;
-                    case GETTEXTFROMWEBELEMENT:
-                        functions.getTextFromElementAtThisCss(checker.getStringValueWithIndex(0));
-                        break;
-                    case CLOSE:
-                        functions.closeFunction();
-                        break;
-
-                    case STARTWEBGETTER:
-                        functions.startWebGetter();
-                        break;                       
-                }
+            if(webFunctionsIsUsed(listToRun)){
+                initiateWebFunctions();
+                runWebFunctions(listToRun);
             }
         } catch (Exception e) {
         }
-
-//  logicInterface.runListOfFunctionInstructionsInThisThread(listToRun);
-        /* for(FunctionsInstruction checker : listToRun.getListOfFunctionInstruction()){
-          try {
-              logicInterface.runFunctionInstruction(checker);
-          } catch (InterruptedException ex) {
-              Logger.getLogger(FunctionsInstructionListRunner.class.getName()).log(Level.SEVERE, null, ex);
-          } catch (AWTException ex) {
-              Logger.getLogger(FunctionsInstructionListRunner.class.getName()).log(Level.SEVERE, null, ex);
-          }
-      }*/
     }
 
+    private boolean webFunctionsIsUsed(ListOfFunctionInstructions listToCheck) {
+        for (FunctionsInstruction checker : listToRun.getListOfFunctionInstruction()) {
+            switch (checker.getCommand()) {
+                case CLICKWEBELEMENT:
+                    return true;
+                case CONNECTTOTHISITE:
+                    return true;
+                case GETTEXTFROMWEBELEMENT:
+                    return true;
+                case STARTWEBGETTER:
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private void initiateWebFunctions() throws AWTException{
+        this.webFunctions = new WebFunction("WebFunctionListRunner");
+    }
+    
+    private void runWebFunctions(ListOfFunctionInstructions listToRun) throws AWTException, InterruptedException{
+         for (FunctionsInstruction checker : listToRun.getListOfFunctionInstruction()) {
+                switch (checker.getCommand()) {
+                    case CLICKWEBELEMENT:
+                        webFunctions.clickElementAtThisCss(checker.getStringValueWithIndex(0));
+                        break;
+                    case CONNECTTOTHISITE:
+                        webFunctions.connectWebGetterToThisSite(checker.getStringValueWithIndex(0));
+                        break;
+                    case GETTEXTFROMWEBELEMENT:
+                        webFunctions.getTextFromElementAtThisCss(checker.getStringValueWithIndex(0));
+                        break;
+                    case CLOSE:
+                        webFunctions.closeFunction();
+                        break;
+                    case STARTWEBGETTER:
+                        webFunctions.startWebGetter();
+                        break;
+                }
+            }     
+    }
 }
+
